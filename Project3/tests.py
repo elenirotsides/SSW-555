d6c5b8b5d83df78b03296c49ce137de28e5cde98
Eleni Rotsides <43190242+elenirotsides@users.noreply.github.com>
2022-02-28 14:53:15 -0500

    merged tests into one file

--- Project3/tests.py
+++ Project3/tests.py
@@ -5,7 +5,7 @@ Unit tests for Project3.py
 """
 
 import unittest
-from Project3 import is_birth_before_marriage
+from Project3 import is_birth_before_marriage, is_divorce_before_death, birth_before_death
 import datetime
 
 
@@ -33,6 +33,60 @@ class TestBirthBeforeMarriage(unittest.TestCase):
     def test_nothing_is_given(self):
         """Returns NA if both arguments are NA"""
         self.assertEqual(is_birth_before_marriage("NA", "NA"), "NA")
+
+
+class TestDivorceBeforeDeath(unittest.TestCase):
+    def test_no_death(self):
+        """Returns True if both spouses have not passed away"""
+        self.assertEqual(is_divorce_before_death(
+            datetime.date(1999, 8, 27), "NA", "NA"), True)
+
+    def test_divorce_before(self):
+        """Returns True if the divorce happened before the death of both spouses"""
+        self.assertEqual(is_divorce_before_death(
+            datetime.date(1999, 8, 27), datetime.date(2005, 8, 27), datetime.date(2010, 8, 27)), True)
+
+    def test_husband_passed(self):
+        """Returns True if the divorce happened before the death of the husband"""
+        self.assertEqual(is_divorce_before_death(
+            datetime.date(1999, 8, 27), datetime.date(1999, 9, 27), "NA"), True)
+
+    def test_wife_passed(self):
+        """Returns True if the divorce happened before the death of the wife"""
+        self.assertEqual(is_divorce_before_death(
+            datetime.date(1999, 8, 27), "NA", datetime.date(1999, 9, 27)), True)
+
+    def test_death_before(self):
+        """Returns False if the divorce happened after the death of both spouses"""
+        self.assertEqual(is_divorce_before_death(
+            datetime.date(1999, 8, 27), datetime.date(1999, 5, 27), datetime.date(1999, 4, 27)), False)
+
+
+class TestBirthBeforeDeath(unittest.TestCase):
+    def test_no_death(self):
+        """Returns NA if individual is not dead"""
+        self.assertEqual(birth_before_death(
+            datetime.date(1999, 8, 27), "NA"), "NA")
+
+    def test_no_birth(self):
+        """Returns NA if individual is not dead"""
+        self.assertEqual(birth_before_death(
+            "NA", datetime.date(1999, 8, 27)), "NA")
+
+    def test_birth_before_death(self):
+        """Returns True if birth before death"""
+        self.assertTrue(birth_before_death(
+            datetime.date(1999, 12, 11), datetime.date(2078, 6, 11)), True)
+
+    def test_birth_before_death2(self):
+        """Returns True if birth before death"""
+        self.assertTrue(birth_before_death(
+            datetime.date(2020, 12, 11), datetime.date(2020, 12, 12)), True)
+
+    def test_death_before_birth(self):
+        """Returns False if birth after death"""
+        self.assertFalse(birth_before_death(
+            datetime.date(1999, 12, 11), datetime.date(1995, 6, 11)), False)
 
 
 if __name__ == '__main__':
