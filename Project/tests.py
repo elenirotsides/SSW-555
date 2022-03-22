5f8563cb48a4208fa25c0f6aaf9c8dc04ac8923f
Eleni Rotsides <43190242+elenirotsides@users.noreply.github.com>
2022-03-22 11:29:58 -0400

    Merge branch 'main' into refactored-divorce_b_death

--- Project/tests.py
+++ Project/tests.py
@@ -72,22 +72,22 @@ class test_birthBeforeMarriage(unittest.TestCase):
     def test_no_marriage(self):
         """Returns NA if marriage doesn't exist"""
         self.assertEqual(userStories.is_birth_before_marriage(
-            datetime.date(1999, 8, 27), "NA"), "NA")
+            "1999-8-27", "NA"), "NA")
 
     def test_no_birth_date(self):
         """Returns NA if birth date doesn't exist"""
         self.assertEqual(userStories.is_birth_before_marriage(
-            "NA", datetime.date(1999, 8, 27)), "NA")
+            "NA", "1999-8-27"), "NA")
 
     def test_birth_is_before_marriage(self):
         """Returns true if birth is before marriage"""
-        self.assertEqual(userStories.is_birth_before_marriage(datetime.datetime(
-            1990, 3, 15), datetime.datetime(2020, 1, 1)), True)
+        self.assertEqual(userStories.is_birth_before_marriage(
+            "1990-3-15", "2020-1-1"), True)
 
     def test_birth_is_not_before_marriage(self):
         """Returns false if birth is not before marriage"""
-        self.assertAlmostEqual(userStories.is_birth_before_marriage(datetime.datetime(2020, 1, 1), datetime.datetime(
-            1990, 3, 15)), False)
+        self.assertAlmostEqual(userStories.is_birth_before_marriage(
+            "2020-1-1", "1990-3-15"), False)
 
     def test_nothing_is_given(self):
         """Returns NA if both arguments are NA"""
