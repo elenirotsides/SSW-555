8134466d7fe0e1940723620d31e3ffb08d81b03e
http-dave <taveras.dave@gmail.com>
2022-03-27 17:55:39 -0400

    Refactored a bit

--- Project/userStories.py
+++ Project/userStories.py
@@ -39,6 +39,7 @@ months = {
 
 def compareDates(date1, date2):
 	#helper function to check if date 1 comes before date 2
+	#Dates provided must be in YYYY-MM-DD format
 
     #create tokens from date 1
     d1_tokens = date1.split("-")
@@ -57,6 +58,7 @@ def compareDates(date1, date2):
             if d1_tokens[2] <= d2_tokens[2]:
                 # Check days
                 return True
+    return False;
 
 
 """
@@ -170,26 +172,8 @@ def marriageBeforeDeath(m_date, d_date):
     if(d_date == "NA"):
         # if person has not died then any marriage date is valid
         return True
-
-    # Creating marriage date from substrings of m_date
-    m_tokens = m_date.split("-")
-
-    # Creating death date from substrings of d_date
-    d_tokens = d_date.split("-")
-
-    if m_tokens[0] < d_tokens[0]:
-        return True
-    if m_tokens[0] == d_tokens[0]:
-        # If years are equal check months
-        if m_tokens[1] < d_tokens[1]:
-            return True
-        if m_tokens[1] == d_tokens[1]:
-            # If months are equal check days
-            if m_tokens[2] <= d_tokens[2]:
-                # Check days
-                return True
-
-    return False
+        
+    return compareDates(m_date, d_date)
 
 
 """
